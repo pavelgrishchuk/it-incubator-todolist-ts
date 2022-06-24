91d5cebbc7c54962983e7b2ed8b075d761a1e739
pavelgrishchuk <paulgrishchuk@gmail.com>
2022-06-24 22:23:05 +0300

    feat: added second Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import './App.css';
+import TodoList, {TaskType} from "./TodoList";
 
 function App() {
+    const title_1: string = "What to learn"
+    const title_2: string = "What to learn"
+
+    const task_1: Array<TaskType> = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS/ES6", isDone: true},
+        {id: 3, title: "REACT", isDone: false},
+    ]
+    const task_2:Array<TaskType> = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "REACT", isDone: false},
+    ]
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList title={title_1} tasks={task_1}/>
+            <TodoList title={title_2} tasks={task_2}/>
+
         </div>
     );
 }
