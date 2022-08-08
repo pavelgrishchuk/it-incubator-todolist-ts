3f1097bf800d9c6f7d065572da855cf876f55f5e
pavelgrishchuk <paulgrishchuk@gmail.com>
2022-08-08 11:08:43 +0300

    L4

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,33 @@ export type FilterValuesType = "all" | "active" | "completed"
 
 function App() {
     console.log(v1())
+    //BLL:
     const title: string = "What to learn"
-
     const [tasks, setTasks] = useState([
         {id: v1(), title: "HTML&CSS", isDone: true},
         {id: v1(), title: "JS/ES6", isDone: true},
         {id: v1(), title: "REACT", isDone: false},
     ])
+    const [filter, setFilter] = useState<FilterValuesType>("all")
+
     const removeTask = (taskID: string) => {
         const updatedTasks = tasks.filter(task => task.id !== taskID)
         setTasks(updatedTasks)
     }
-
     const addTask = (title: string) => {
         const id = v1()
         const isDone = false
         setTasks([{id, title, isDone}, ...tasks])
     }
+    const changeFilter = (filter: FilterValuesType) => {
+        setFilter(filter)
+    }
+    const changeTaskStatus = (taskID: string, isDone: boolean) => { // 3, false
+        setTasks(tasks.map(t => t.id ===taskID ? {...t, isDone: isDone} :t))
+      
+    }
 
-    const [filter, setFilter] = useState<FilterValuesType>("all")
+    //UI:
     let tasksForRender;
     switch (filter) {
         case "completed":
@@ -37,12 +45,6 @@ function App() {
         default:
             tasksForRender = tasks
     }
-
-    const changeFilter = (filter: FilterValuesType) => {
-        setFilter(filter)
-
-    }
-
     return (
         <div className="App">
             <TodoList
@@ -55,5 +57,4 @@ function App() {
         </div>
     );
 }
-
 export default App;
